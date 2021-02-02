55df139acb4208e51cb9312abc92a637e6f7f600
Shashank Pedamallu <spedamallu@lyft.com>
2021-02-02 08:45:35 -0800

    Added test that exceeds Time limit

--- src/strings/MaximumGainReplaceSubstrings.java
+++ src/strings/MaximumGainReplaceSubstrings.java
@@ -5,14 +5,15 @@ public class MaximumGainReplaceSubstrings {
         return backtrack(s, x, y, 0);
     }
     
-    private int backtrack(String s, int x, int y, int max) {
+    private int backtrack(String s, int x, int y, int maxPrev) {
         int i = s.indexOf("ab");
         int j = s.indexOf("ba");
+        int max = maxPrev;
         if(i>=0 && i<s.length()-1) {
-            max = Math.max(backtrack(s.substring(0,i)+s.substring(i+2), x, y, max+x), max);
+            max = Math.max(backtrack(s.substring(0,i)+s.substring(i+2), x, y, maxPrev+x), max);
         } 
         if (j>=0 && j<s.length()-1) {
-            max = Math.max(backtrack(s.substring(0,j)+s.substring(j+2), x, y, max+y), max);
+            max = Math.max(backtrack(s.substring(0,j)+s.substring(j+2), x, y, maxPrev+y), max);
         }
         return max;
     }
--- test/strings/MaximumGainReplaceSubstringsTest.java
+++ test/strings/MaximumGainReplaceSubstringsTest.java
@@ -19,4 +19,21 @@ public class MaximumGainReplaceSubstringsTest {
 		Assert.assertEquals(19, obj.maximumGain(testString, 4, 5));
 	}
 
+	@Test
+	public void test2() {
+		String testString = "aabbaaxybbaabb";
+		Assert.assertEquals(20, obj.maximumGain(testString, 5, 4));
+	}
+
+	@Test
+	public void test3() {
+		String testString = "aba";
+		Assert.assertEquals(5, obj.maximumGain(testString, 4, 5));
+	}
+	
+	@Test
+	public void test4_time_limit() {
+		String testString = "aabbrtababbabmaaaeaabeawmvaataabnaabbaaaybbbaabbabbbjpjaabbtabbxaaavsmmnblbbabaeuasvababjbbabbabbasxbbtgbrbbajeabbbfbarbagha";
+		Assert.assertEquals(198644, obj.maximumGain(testString, 8484, 4096));
+	}
 }
