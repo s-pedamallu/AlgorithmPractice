530fa9cbec3b036748ed64c05c34b6de8e3b7894
Shashank Pedamallu <spedamallu@lyft.com>
2021-02-01 20:40:55 -0800

    Successful iteration

--- src/arrays/SquirrelDistance.java
+++ src/arrays/SquirrelDistance.java
@@ -4,28 +4,22 @@ package arrays;
 public class SquirrelDistance {
     public int minDistance(int height, int width, int[] tree, int[] squirrel, int[][] nuts) {
         int allNutsTreesDist = 0;
-        int minSquirrelNut = Integer.MAX_VALUE;
+        int minOffset = Integer.MAX_VALUE;
         int minNut = -1;
         for (int i=0; i<nuts.length; i++) {
             allNutsTreesDist += 2*(distBtwPoints(tree, nuts[i]));
-            int squirrelDist = distBtwPoints(squirrel, nuts[i]);
-            System.out.println("i - "+i+" ; dT - " + distBtwPoints(tree, nuts[i]) +
-            		" ; dS - "+squirrelDist+" ; dST - " + distSNT(squirrel, tree, nuts[i]));
+            int offset = distBtwPoints(squirrel, nuts[i]) - distBtwPoints(tree, nuts[i]);
             if(tree[0]==nuts[i][0] && tree[1]==nuts[i][1]) {
             	continue; // same location as tree. skip
-            } else if (squirrelDist < minSquirrelNut) { 
-                minSquirrelNut = squirrelDist;
+            } else if ( (minNut == -1) || (minOffset > offset)) { 
+                minOffset = offset;
                 minNut = i;
-            } else if ((squirrelDist == minSquirrelNut) && 
-            		(distBtwPoints(nuts[minNut], tree) < distBtwPoints(tree, nuts[i]))){
-                minSquirrelNut = squirrelDist;
-                minNut = i;            	
             }
         }
         if(minNut == -1) {
             return 0;
         }
-        return (allNutsTreesDist + minSquirrelNut - distBtwPoints(tree, nuts[minNut]));                
+        return (allNutsTreesDist + minOffset);                
     }
     
     private int distBtwPoints(int[] a, int[] b) {
