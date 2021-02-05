55f9d0e2296ebf6929294ee20dcb8a192ae19fb0
Shashank Pedamallu <spedamallu@lyft.com>
2021-02-04 23:00:46 -0800

    solved

--- test/trees/NumEdgesBetweenNodesTest.java
+++ test/trees/NumEdgesBetweenNodesTest.java
@@ -22,4 +22,32 @@ public class NumEdgesBetweenNodesTest {
 		TreeNode root = TestTreeHelper.buildTreeFromArray(bst);
 		Assert.assertEquals(3, obj.findDistance(root, 5, 0));
 	}
+	
+	@Test
+	public void test2() {
+		Integer[] bst = {3,5,1,6,2,0,8,null,null,7,4};
+		TreeNode root = TestTreeHelper.buildTreeFromArray(bst);
+		Assert.assertEquals(2, obj.findDistance(root, 5, 4));
+	}
+
+	@Test
+	public void test3() {
+		Integer[] bst = {3,5,1,6,2,0,8,null,null,7,4};
+		TreeNode root = TestTreeHelper.buildTreeFromArray(bst);
+		Assert.assertEquals(4, obj.findDistance(root, 6, 8));
+	}
+
+	@Test
+	public void test4() {
+		Integer[] bst = {3,5,1,6,2,0,8,null,null,7,4};
+		TreeNode root = TestTreeHelper.buildTreeFromArray(bst);
+		Assert.assertEquals(3, obj.findDistance(root, 6, 7));
+	}
+
+	@Test
+	public void test5() {
+		Integer[] bst = {1,2,null,null,3,4,null,5,null,6,null,null,7,null,20,8,null,10,null,9,null,11,null,12,null,13};
+		TreeNode root = TestTreeHelper.buildTreeFromArray(bst);
+		Assert.assertEquals(2, obj.findDistance(root, 20, 10));
+	}
 }
