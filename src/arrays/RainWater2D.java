9e4c830f567d7bf71ff620a0aba2781c1127a5e0
Shashank Pedamallu <spedamallu@lyft.com>
2021-02-18 01:29:31 -0800

    more optimized when saw discussion

--- src/arrays/RainWater2D.java
+++ src/arrays/RainWater2D.java
@@ -6,8 +6,31 @@ import java.util.Stack;
  * Problem: https://leetcode.com/problems/trapping-rain-water/
  */
 public class RainWater2D {
+	public int trap(int[] height) {
+		return trapOptimized(height);
+	}
+	
+	private int trapOptimized(int[] height) {
+		int left = 0;
+		int right = height.length-1;
+		int lMax = Integer.MIN_VALUE;
+		int rMax = Integer.MIN_VALUE;
+		int ans = 0;
+		while(right-left>0) {
+            lMax = Math.max(lMax, height[left]);
+            rMax = Math.max(rMax, height[right]);
+			if(height[left] <= height[right]) {
+				ans += Math.min(lMax, rMax) - height[left];
+				left++;
+			} else {
+				ans += Math.min(lMax, rMax) - height[right];
+				right--;
+			}
+		}
+		return ans;
+	}
 
-    public int trap(int[] height) {
+    private int trapStack(int[] height) {
         int ans = 0;
         Stack<Integer> stack = new Stack<>();
         stack.push(0);
