dcb104396aa05218f8616bedf4ea17e3a302f349
Shashank Pedamallu <spedamallu@lyft.com>
2021-02-18 01:03:45 -0800

    testing locally

---
+++ src/arrays/RainWater2D.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Stack;
+
+/**
+ * Problem: https://leetcode.com/problems/trapping-rain-water/
+ */
+public class RainWater2D {
+
+    public int trap(int[] height) {
+        int ans = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for(int i=1; i<height.length; i++) {
+        	int popIndex = -1;
+            while(!stack.isEmpty() && height[stack.peek()] <= height[i]) {
+                popIndex = stack.pop();
+            }
+            if(popIndex != -1) {
+                int fillHeight=Math.min(height[i], height[popIndex]);
+                for(int j=popIndex+1; j<i;j++) {
+                    ans += fillHeight - height[j];
+                    height[j] = fillHeight;
+                }                
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+    
+}
---
+++ test/arrays/RainWater2DTest.java
@@ -0,0 +1,40 @@
+package arrays;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RainWater2DTest {
+
+	private RainWater2D obj;
+	
+	@Before
+	public void setup() {
+		obj = new RainWater2D();
+	}
+	
+	@Test
+	public void test1() {
+		int[] arr = {1,8,6,2,5,4,8,3,7};
+		assertEquals(19, obj.trap(arr));
+	}
+	
+	@Test
+	public void test2() {
+		int[] arr = {1,1};
+		assertEquals(0, obj.trap(arr));
+	}
+	
+	@Test
+	public void test3() {
+		int[] arr = {4,2,0,3,2,5};
+		assertEquals(9, obj.trap(arr));
+	}
+
+	@Test
+	public void test4() {
+		int[] arr = {0,1,0,2,1,0,1,3,2,1,2,1};
+		assertEquals(6, obj.trap(arr));
+	}
+}
